93408a3ac4d1ce636403e92747a62daf51012877
Displee <displee@hotmail.com>
2021-01-26 21:35:19 +0100

    Add TODO.

--- src/app/shared/file-preview/model-file-preview/model-file-preview.service.ts
+++ src/app/shared/file-preview/model-file-preview/model-file-preview.service.ts
@@ -76,6 +76,7 @@ export class ModelFilePreviewService implements OnDestroy {
     }
 
     public createRsModelMesh(rsModel: RsModel): void {
+        // TODO Draw mesh
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
         const cube = new THREE.Mesh(geometry, material);
